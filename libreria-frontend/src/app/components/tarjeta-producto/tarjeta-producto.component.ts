7e7108a767d74481c994ae803e64eb4663c3ec04
Erick <erickvillaseor@yahoo.com.mx>
2025-07-04 23:34:32 -0600

    Ya esta chiquita la imagen

--- libreria-frontend/src/app/components/tarjeta-producto/tarjeta-producto.component.ts
+++ libreria-frontend/src/app/components/tarjeta-producto/tarjeta-producto.component.ts
@@ -3,12 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-tarjeta-producto',
   standalone: false,
-  templateUrl: './tarjeta-producto.component.html'
+  templateUrl: './tarjeta-producto.component.html',
+  styleUrls: ['./tarjeta-producto.component.css']
 })
 export class TarjetaProductoComponent {
   @Input() libro: any;
 
   handleImageError(event: any) {
     event.target.src = 'assets/imagenes/libro-default.jpg';
+    event.target.style.width = '120px';
+    event.target.style.height = '160px';
   }
 }
