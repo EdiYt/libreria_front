b758d448adfc17393db9777eb01c43d787c07882
Erick <erickvillaseor@yahoo.com.mx>
2025-07-04 18:18:28 -0600

    Se agrego el ts y html de cabecera

--- libreria-frontend/src/app/components/cabecera/cabecera.component.ts
+++ libreria-frontend/src/app/components/cabecera/cabecera.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cabecera',
-  imports: [],
   templateUrl: './cabecera.component.html',
-  styleUrl: './cabecera.component.css'
 })
-export class CabeceraComponent {
 
-}
+export class CabeceraComponent { titulo = 'Librería Erick'; }
