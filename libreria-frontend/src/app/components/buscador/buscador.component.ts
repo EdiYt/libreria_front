3b9f84330351af7cda05af0f767144909c035a35
Erick <erickvillaseor@yahoo.com.mx>
2025-07-04 19:17:01 -0600

    Se agrego el html y ts de el buscador pero tiene un error

--- libreria-frontend/src/app/components/buscador/buscador.component.ts
+++ libreria-frontend/src/app/components/buscador/buscador.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-@Component({
-  selector: 'app-buscador',
-  imports: [],
-  templateUrl: './buscador.component.html',
-  styleUrl: './buscador.component.css'
-})
+@Component({ selector: 'app-buscador', templateUrl: './buscador.component.html' })
 export class BuscadorComponent {
+  @Input() generos: any[] = [];
+  @Output() onBuscar  = new EventEmitter<string>();
+  @Output() onFiltrar = new EventEmitter<number>();
 
+  termino = '';
+
+  buscar() { this.onBuscar.emit(this.termino); }
+
+  filtrarPorGenero(e: Event) {
+    const id = Number((e.target as HTMLSelectElement).value);
+    this.onFiltrar.emit(id);
+  }
 }
